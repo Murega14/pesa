d35325928b40829e4e39d3fd5f10f797e20fbfce
Bravin Murega <tedmurega@gmail.com>
2023-03-16 21:37:24 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/pesa/MainActivity.java
+++ app/src/main/java/com/example/pesa/MainActivity.java
@@ -1,35 +1,19 @@
 package com.example.pesa;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.pesa.ui.login.loginActivity;
+import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
-    // define the variable
-    Button login, signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        login = findViewById(R.id.login);
-        signup = findViewById(R.id.signup);
-
-        // add the OnClickListener in sender button after clicked this button following Instruction will run
-        login.setOnClickListener(v -> {
-            // Create the Intent object of this class Context() to Second_activity class
-            Intent intent = new Intent(getApplicationContext(), loginActivity.class);
-            // start the Intent
-            startActivity(intent);
-        });
-        signup.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-            startActivity(intent);
-        });
-}}
+        FirebaseApp.initializeApp(this);
+    }
+}
